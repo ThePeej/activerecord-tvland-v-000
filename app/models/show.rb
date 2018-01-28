afcf3964c13f5321ec362ef76489581b6d4dd481
PJ Frias <peejfrias@gmail.com>
2018-01-28 02:32:43 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :shows
 
-  def build_network
-
+  def build_network(network)
+    Network.new(network)
   end
 end
